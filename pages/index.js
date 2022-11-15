bb250db53a53bbb1485eb5daa6f7c09362428212
Kevin <kevinfiorentino.1294@gmail.com>
2022-11-15 16:05:04 -0300

    Wallet connect

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,40 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 const App = () => {
+
+  const WalletMultiButtonDynamic = dynamic(
+    async () =>
+      (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+  );
   
-  
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src='https://media4.giphy.com/media/l0MYKMrQnwNvLjYhW/giphy.gif?cid=ecf05e47tu4c6ypa604nj709idisnd4iuhfliiohjmbxlkhk&rid=giphy.gif&ct=g' alt='The Dark Side of The Moon' />
+      <div className='button-container'>
+        <WalletMultiButtonDynamic className='cta-button connect-wallet-button' />
+      </div>    
+    </div>
+  );
+
   return (
-    <div className="App">
-      <HeadComponent/>
-      <div className="container">
-        <header className="header-container">
-          <p className="header"> —△🌈 Pink Floyd GIF Store</p>
-          <p className="sub-text">The Dark Side Of The Store</p>
+    <div className='App'>
+      <div className='container'>
+        <header className='header-container'>
+          <p className='header'> —△🌈 Pink Floyd GIF Store</p>
+          <p className='sub-text'>The Dark Side Of The Store</p>
         </header>
 
         <main>
-          <img src="https://media4.giphy.com/media/l0MYKMrQnwNvLjYhW/giphy.gif?cid=ecf05e47tu4c6ypa604nj709idisnd4iuhfliiohjmbxlkhk&rid=giphy.gif&ct=g" alt="The Dark Side of The Moon" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
         </main>
 
-        <div className="footer-container">
+        <div className='footer-container'>
         </div>
       </div>
     </div>
