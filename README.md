790a4ca8767d5732c06bb419ffa1be1cf66cfde1
Kevin <kevinfiorentino.1294@gmail.com>
2022-11-23 19:04:09 -0300

    Owner project add product

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React/Solana Pay
 
+Buy and sell images with USDC and Phantom
+
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect} from "react";
+import CreateProduct from "../components/CreateProduct";
 import Product from "../components/Product";
+import HeadComponent from '../components/Head';
 
-import { useWallet } from '@solana/wallet-adapter-react';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 const App = () => {
 
   const { publicKey } = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -21,8 +25,11 @@ const App = () => {
   }, [publicKey]);
 
   const renderNotConnectedContainer = () => (
-    <div className="button-container">
-      <WalletMultiButton className="cta-button connect-wallet-button" />
+    <div>
+      <img src="https://media4.giphy.com/media/l0MYKMrQnwNvLjYhW/giphy.gif?cid=ecf05e47tu4c6ypa604nj709idisnd4iuhfliiohjmbxlkhk&rid=giphy.gif&ct=g" alt="The Dark Side of The Moon" />
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
     </div>
   );
 
@@ -35,14 +42,22 @@ const App = () => {
   );
 
   return (
-    <div className='App'>
-      <div className='container'>
+    <div className="App">
+      <HeadComponent/>
+      <div className="container">
         <header className='header-container'>
           <p className='header'> —△🌈 Pink Floyd GIF Store</p>
           <p className='sub-text'>The Dark Side Of The Store</p>
+
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
         </header>
 
         <main>
+          {creating && <CreateProduct />}
           {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
